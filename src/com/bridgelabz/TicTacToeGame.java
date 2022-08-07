acbb2750bf86ce731fc01230e440570419ecfac2
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-07 09:22:06 +0530

    Refactored:Method ShowBoard To Display Board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,33 +3,40 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	
+
 	static char[] board = new char[10];
 	static char player,computer;
 	public static Scanner scanner = new Scanner(System.in);
-	
+
 	static void createBoard(){
 		for(int index = 1 ; index<10 ; index++) {
 			board[index] = ' ';
 		}
 	}
-	
+
 	static void getPlayerChoice() {
-		 System.out.print("select X or O : ");
-		 player = Character.toUpperCase(scanner.next().charAt(0));
-		 
-		 if (player == 'X')
-			 computer='O';
-		 else
-			 computer ='X';
-		 System.out.println("You have selected : " +player);
-		 System.out.println("Computer's choice : " +computer);
+		System.out.print("select X or O : ");
+		player = Character.toUpperCase(scanner.next().charAt(0));
+
+		if (player == 'X')
+			computer='O';
+		else
+			computer ='X';
+		System.out.println("You have selected : " +player);
+		System.out.println("Computer's choice is : " +computer);
 	}
-	
+
+	static void showBoard() {
+		System.out.println("  " + board[1] + "  |  " + board[2]  + "   | " + board[3] + "  ");
+		System.out.println("  " + board[4] + "  |  " + board[5]  + "   | " + board[6] + "  ");
+		System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
+	}
+
 	public static void main(String[] args) {
+
 		System.out.println("Welcome To Tic Tac Toe Game");
 		createBoard();
 		getPlayerChoice();
-		
+
 	}
 }
